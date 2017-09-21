b5b2104cf1b265fb77fe4a75bc1ee37a57f6ab28
Laura Micek <Laura.Micek@fmr.com>
2017-09-21 12:53:01 -0700

    cleaning again

--- src/main/java/com/echo/forex/services/LimitOrderService.java
+++ src/main/java/com/echo/forex/services/LimitOrderService.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Service;
 
 import com.echo.forex.entities.CurrencyPairs;
 import com.echo.forex.entities.LimitOrder;
-import com.echo.forex.entities.OrderStatus;
 import com.echo.forex.entities.Transaction;
 import com.echo.forex.repositories.LimitOrderRepo;
 
@@ -50,14 +49,14 @@ public class LimitOrderService {
 		return lmOrder.getId();
 	}
 	
-	public String cancelLimitOrderById(long id) {
+	/*public String cancelLimitOrderById(long id) {
 		LimitOrder changeLmtOrder = getLimitOrderById(id);
 		if(changeLmtOrder.getOrderState() == OrderStatus.Open) {
 			changeLmtOrder.setOrderState(OrderStatus.Canceled);
 			this.limitOrderMart.save(changeLmtOrder);
 		} 
 		return "The order status is " + changeLmtOrder.getOrderState();
-	}
+	}*/
 	
 	
 }
